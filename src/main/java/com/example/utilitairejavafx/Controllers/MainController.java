e6c4211a80c63b36665224cabe8e74e8d28a0a4a
Philémon Wild <philemon.wild@gmail.com>
2021-12-08 15:33:00 +0100

    logo test

--- src/main/java/com/example/utilitairejavafx/Controllers/MainController.java
+++ src/main/java/com/example/utilitairejavafx/Controllers/MainController.java
@@ -3,6 +3,7 @@ package com.example.utilitairejavafx.Controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.layout.VBox;
@@ -52,7 +53,20 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //on récupère les images pour en faire des logos
+//        ImageView imgLibrary = new ImageView(getClass().getResource("src/main/resources/com/example/utilitairejavafx/LogoBook.jpeg").toExternalForm());
+//        ImageView imgArmy = new ImageView(getClass().getResource("src/main/resources/com/example/utilitairejavafx/ArmeeLogo.jpeg").toExternalForm());
+//        ImageView imgImc = new ImageView(getClass().getResource("src/main/resources/com/example/utilitairejavafx/imc-logo.png").toExternalForm());
+//        ImageView imgConvert = new ImageView(getClass().getResource("src/main/resources/com/example/utilitairejavafx/convertIcon.png").toExternalForm());
+
+        //on efface la zone centrale
         containerCentral.getChildren().removeAll(imcContainer,armySimContainer,converterContainer,tabBiblio);
+
+//        btnConvert.setGraphic(imgConvert);
+//        btnSim.setGraphic(imgArmy);
+//        btnIMC.setGraphic(imgImc);
+//        btnLibrary.setGraphic(imgLibrary);
+
         btnConvert.setOnMouseClicked(btnaction -> {
             //affiche la vbox correspondante au bouton
             containerCentral.getChildren().removeAll(imcContainer,armySimContainer,converterContainer,tabBiblio);
