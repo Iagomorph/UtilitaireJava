e673accbf87e10862055511024ebb0a9b876d495
Jules <jules.duarte@edu.esiee-it.fr>
2021-12-09 10:04:35 +0100

    contextMenu

--- src/main/java/com/example/utilitairejavafx/Controllers/ArmySimController.java
+++ src/main/java/com/example/utilitairejavafx/Controllers/ArmySimController.java
@@ -2,6 +2,8 @@ package com.example.utilitairejavafx.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeView;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,15 +11,25 @@ import java.util.ResourceBundle;
 import javafx.scene.control.TreeItem;
 
 public class ArmySimController implements Initializable {
+
     @FXML
     private TreeView<?> armyTree;
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TreeItem<String> army = new TreeItem<> ("Mon Armée");
         army.setExpanded(true);
 
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem menuItem1 = new MenuItem("Choice 1");
+        MenuItem menuItem2 = new MenuItem("Choice 2");
+        MenuItem menuItem3 = new MenuItem("Choice 3");
+
+
+        contextMenu.getItems().addAll(menuItem1,menuItem2,menuItem3);
+
+        armyTree.setContextMenu(contextMenu);
 //        armyTree.setRoot();
     }
 }
